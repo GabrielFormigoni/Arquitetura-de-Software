43cd19c7a494ce9a5f145cf6d24f7c0221e25cce
Gabriel Formigoni <bielformigoni@gmail.com>
2023-08-13 20:59:34 -0300

    fix

--- movies_app/nuxt.config.js
+++ movies_app/nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
 
-  target: 'static',
   head: {
     title: 'nuxtmovieapp',
     htmlAttrs: {
@@ -97,5 +96,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    buildDir: 'dist',
+  },
 }
